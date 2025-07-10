477bbd4094c6c64ea8e28b5e246820002648e1f0
Qianye Su <976113615@qq.com>
2025-07-10 16:21:12 +0800

    fix: Simplify build_docs.bat by removing unnecessary echo statements and comments fix: Clean up index.rst by removing the caption from the toctree directive

--- docs/build_docs.bat
+++ docs/build_docs.bat
@@ -1,18 +1,6 @@
 @echo off
-REM Build script for Windows
 echo Building Skyborn documentation...
-
+python "%~dp0build_docs.py" %*
 echo.
-echo Building English documentation...
-sphinx-build -b html source/en docs/build/html/en
-
-echo.
-echo Building Chinese documentation...
-sphinx-build -b html source/zh_CN docs/build/html/zh_CN
-
-echo.
-echo Documentation built successfully!
-echo English docs: docs/build/html/en/index.html
-echo Chinese docs: docs/build/html/zh_CN/index.html
-
+echo Documentation build process finished.
 pause
