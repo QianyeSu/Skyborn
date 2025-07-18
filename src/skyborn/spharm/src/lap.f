d7b049a324d31668d61221107cd6e96f811c2236
Qianye Su <976113615@qq.com>
2025-07-19 02:33:34 +0800

    refactor: optimize invlap and lap subroutines for improved performance and readability

--- src/skyborn/spharm/src/lap.f
+++ src/skyborn/spharm/src/lap.f
@@ -1,47 +1,21 @@
-!
-! Optimized version of lap.f
-! Computes Laplacian operator in spectral space
-! Optimizations: Modern Fortran syntax, vectorization, reduced function calls
-!
-subroutine lap(dataspec, dataspec_lap, nmdim, nt, rsphere)
-    implicit none
+      subroutine lap(dataspec,dataspec_lap,nmdim,nt,rsphere)
+      integer nmdim
+      complex dataspec(nmdim,nt),dataspec_lap(nmdim,nt)
+      real rsphere
 
-    ! Input/Output parameters
-    integer, intent(in) :: nmdim, nt
-    real, intent(in) :: rsphere
-    complex, intent(in) :: dataspec(nmdim, nt)
-    complex, intent(out) :: dataspec_lap(nmdim, nt)
+      ntrunc = -1.5 + 0.5*sqrt(9.-8.*(1.-float(nmdim)))
 
-    ! Local variables
-    integer :: ntrunc, m, n, nm, nmstrt, i
-    real :: rsphere_inv_sq, n_real, lap_factor
+      do i=1,nt
+      nmstrt = 0
+      do m=1,ntrunc+1
+      do n=m,ntrunc+1
+         nm = nmstrt + n - m + 1
+         dataspec_lap(nm,i) =
+     *   -(float(n)*float(n-1)/rsphere**2)*dataspec(nm,i)
+      enddo
+      nmstrt = nmstrt + ntrunc - m + 2
+      enddo
+      enddo
 
-    ! Constants
-    rsphere_inv_sq = 1.0 / (rsphere * rsphere)
-
-    ! Calculate truncation from nmdim
-    ntrunc = int(-1.5 + 0.5 * sqrt(9.0 - 8.0 * (1.0 - real(nmdim))))
-
-    ! Main computation loops - optimized for vectorization
-    !$OMP PARALLEL DO PRIVATE(nmstrt, m, n, nm, n_real, lap_factor) &
-    !$OMP             SHARED(ntrunc, rsphere_inv_sq)
-    do i = 1, nt
-        nmstrt = 0
-        do m = 1, ntrunc + 1
-            !DIR$ VECTOR ALWAYS
-            do n = m, ntrunc + 1
-                nm = nmstrt + n - m + 1
-                n_real = real(n)
-
-                ! Pre-compute Laplacian factor
-                lap_factor = -n_real * (n_real - 1.0) * rsphere_inv_sq
-
-                ! Apply Laplacian operator
-                dataspec_lap(nm, i) = lap_factor * dataspec(nm, i)
-            end do
-            nmstrt = nmstrt + ntrunc - m + 2
-        end do
-    end do
-    !$OMP END PARALLEL DO
-
-end subroutine lap
+      return
+      end
