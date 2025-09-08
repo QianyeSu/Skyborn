efd0ae50a0e06fb6cf8167a8e9eee0377d7d45cb
Qianye Su <976113615@qq.com>
2025-09-08 20:48:38 +0800

    add GPI test file[skip ci]

---
+++ tests/GPI/__init__.py
@@ -0,0 +1,8 @@
+"""
+Test suite for the GPI (Genesis Potential Index) module.
+
+This package contains comprehensive tests for all GPI functionality including:
+- Interface tests for potential intensity calculations
+- XArray interface tests
+- Integration tests with various data formats
+"""
