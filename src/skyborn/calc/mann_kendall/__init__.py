3784dc49b2b4466d02a32e6db2641801c611a2eb
Qianye Su <976113615@qq.com>
2026-04-19 23:39:08 +0800

    feat: update __all__ exports to include partial Mann-Kendall functions

--- src/skyborn/calc/mann_kendall/__init__.py
+++ src/skyborn/calc/mann_kendall/__init__.py
@@ -8,11 +8,23 @@ from .core import (
     mk_test,
     trend_analysis,
 )
+from .partial import (
+    partial_mann_kendall_multidim,
+    partial_mann_kendall_test,
+    partial_mann_kendall_xarray,
+    partial_multidim,
+    partial_test,
+)
 
 __all__ = [
     "mann_kendall_test",
     "mann_kendall_multidim",
     "mann_kendall_xarray",
+    "partial_mann_kendall_test",
+    "partial_mann_kendall_multidim",
+    "partial_mann_kendall_xarray",
+    "partial_test",
+    "partial_multidim",
     "trend_analysis",
     "mk_test",
     "mk_multidim",
