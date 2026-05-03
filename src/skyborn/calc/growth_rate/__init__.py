92fd83ab1c2ecc586ec94bec988a0bab206599a2
Qianye Su <976113615@qq.com>
2026-05-03 20:00:57 +0800

    feat: add __init__.py for growth-rate diagnostics and kernel exports

---
+++ src/skyborn/calc/growth_rate/__init__.py
@@ -0,0 +1,8 @@
+"""Growth-rate diagnostics and compiled kernels for `skyborn.calc`."""
+
+from .core import baroc_growth_rate, barot_growth_rate
+
+__all__ = [
+    "baroc_growth_rate",
+    "barot_growth_rate",
+]
