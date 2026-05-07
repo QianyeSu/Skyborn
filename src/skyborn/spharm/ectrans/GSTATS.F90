0f2ef1ab5eb4138f1185620851bbc13752e678bc
Qianye Su <976113615@qq.com>
2026-05-07 20:42:39 +0800

    feat: add GSTATS subroutine to handle KID and KMODE parameters

---
+++ src/skyborn/spharm/ectrans/GSTATS.F90
@@ -0,0 +1,8 @@
+subroutine GSTATS(KID, KMODE)
+  use PARKIND1, only : JPIM
+  implicit none
+
+  integer(kind=JPIM), intent(in) :: KID
+  integer(kind=JPIM), intent(in) :: KMODE
+
+end subroutine GSTATS
