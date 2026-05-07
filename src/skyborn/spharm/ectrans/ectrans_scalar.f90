fd086d43ab3a785680b4e594da81a6461ecfa25a
Qianye Su <976113615@qq.com>
2026-05-07 10:13:02 +0800

    feat: enhance scalar_backend with caching mechanisms and improved scalar_analysis_stub

--- src/skyborn/spharm/ectrans/ectrans_scalar.f90
+++ src/skyborn/spharm/ectrans/ectrans_scalar.f90
@@ -8,6 +8,15 @@ module scalar_backend
   public :: scalar_fourier_stub
   public :: scalar_block_solve_stub
 
+  integer(c_int), save :: basis_cache_ready = 0_c_int
+  integer(c_int), save :: cache_ndgl = 0_c_int
+  integer(c_int), save :: cache_ngptot = 0_c_int
+  integer(c_int), save :: cache_ntrunc = -1_c_int
+  integer(c_int), allocatable, save :: cache_nloen(:)
+  real(c_double), allocatable, save :: cache_mu(:)
+  real(c_double), allocatable, save :: cache_basis_real(:)
+  real(c_double), allocatable, save :: cache_basis_imag(:)
+
   interface
     subroutine getlegfunc(legfunc, lat, ntrunc)
       integer, intent(in) :: ntrunc
@@ -160,9 +169,11 @@ contains
   end subroutine scalar_fourier_stub
 
   subroutine scalar_analysis_stub( &
-    ndgl, nloen, ngptot, ntrunc, nt, datagrid, dataspec_packed, ierror) bind(C)
+    ndgl, nloen, mu, weights, ngptot, ntrunc, nt, datagrid, dataspec_packed, ierror) bind(C)
     integer(c_int), value, intent(in) :: ndgl
     integer(c_int), intent(in) :: nloen(ndgl)
+    real(c_double), intent(in) :: mu(ndgl)
+    real(c_double), intent(in) :: weights(ndgl)
     integer(c_int), value, intent(in) :: ngptot
     integer(c_int), value, intent(in) :: ntrunc
     integer(c_int), value, intent(in) :: nt
@@ -170,8 +181,104 @@ contains
     real(c_double), intent(out) :: dataspec_packed(((ntrunc + 1_c_int) * (ntrunc + 2_c_int)) * nt)
     integer(c_int), intent(out) :: ierror
 
+    integer(c_int) :: ncoeff, nspec2, m, nblock, start_idx
+    integer(c_int) :: ilat, icol, it, coeff_index, ierr_local
+    real(c_double), allocatable :: observed_real(:), observed_imag(:)
+    real(c_double), allocatable :: basis_block_real(:), basis_block_imag(:)
+    real(c_double), allocatable :: observed_block_real(:), observed_block_imag(:)
+    real(c_double), allocatable :: solution_real(:), solution_imag(:)
+
+    if (ndgl < 1_c_int .or. ngptot < 1_c_int .or. nt < 1_c_int .or. ntrunc < 0_c_int) then
+      dataspec_packed = 0.0_c_double
+      ierror = 1_c_int
+      return
+    end if
+
+    ncoeff = (ntrunc + 1_c_int) * (ntrunc + 2_c_int) / 2_c_int
+    nspec2 = 2_c_int * ncoeff
+
+    call ensure_scalar_basis_cache(ndgl, nloen, mu, ngptot, ntrunc, ierr_local)
+    if (ierr_local /= 0_c_int) then
+      dataspec_packed = 0.0_c_double
+      ierror = ierr_local
+      return
+    end if
+
+    allocate(observed_real(ndgl * (ntrunc + 1_c_int) * nt))
+    allocate(observed_imag(ndgl * (ntrunc + 1_c_int) * nt))
+    allocate(basis_block_real(ndgl * (ntrunc + 1_c_int)))
+    allocate(basis_block_imag(ndgl * (ntrunc + 1_c_int)))
+    allocate(observed_block_real(ndgl * nt))
+    allocate(observed_block_imag(ndgl * nt))
+    allocate(solution_real((ntrunc + 1_c_int) * nt))
+    allocate(solution_imag((ntrunc + 1_c_int) * nt))
+
     dataspec_packed = 0.0_c_double
-    ierror = -1_c_int
+
+    call scalar_fourier_stub( &
+      ndgl, nloen, ngptot, ntrunc, nt, datagrid, observed_real, observed_imag, ierr_local)
+    if (ierr_local /= 0_c_int) then
+      dataspec_packed = 0.0_c_double
+      ierror = ierr_local
+      deallocate( &
+        observed_real, observed_imag, basis_block_real, basis_block_imag, &
+        observed_block_real, observed_block_imag, solution_real, solution_imag)
+      return
+    end if
+
+    start_idx = 1_c_int
+    do m = 0_c_int, ntrunc
+      nblock = ntrunc - m + 1_c_int
+
+      do ilat = 1_c_int, ndgl
+        do icol = 1_c_int, nblock
+          coeff_index = start_idx + icol - 1_c_int
+          basis_block_real((ilat - 1_c_int) * nblock + icol) = &
+            cache_basis_real((ilat - 1_c_int) * ncoeff + coeff_index)
+          basis_block_imag((ilat - 1_c_int) * nblock + icol) = &
+            cache_basis_imag((ilat - 1_c_int) * ncoeff + coeff_index)
+        end do
+        do it = 1_c_int, nt
+          observed_block_real((ilat - 1_c_int) * nt + it) = &
+            observed_real(((ilat - 1_c_int) * (ntrunc + 1_c_int) + m) * nt + it)
+          observed_block_imag((ilat - 1_c_int) * nt + it) = &
+            observed_imag(((ilat - 1_c_int) * (ntrunc + 1_c_int) + m) * nt + it)
+        end do
+      end do
+
+      call scalar_block_solve_stub( &
+        ndgl, nblock, nt, weights, &
+        basis_block_real(1:ndgl * nblock), &
+        basis_block_imag(1:ndgl * nblock), &
+        observed_block_real, observed_block_imag, &
+        solution_real(1:nblock * nt), &
+        solution_imag(1:nblock * nt), ierr_local)
+      if (ierr_local /= 0_c_int) then
+        dataspec_packed = 0.0_c_double
+        ierror = ierr_local
+        deallocate( &
+          observed_real, observed_imag, basis_block_real, basis_block_imag, &
+          observed_block_real, observed_block_imag, solution_real, solution_imag)
+        return
+      end if
+
+      do icol = 1_c_int, nblock
+        coeff_index = start_idx + icol - 1_c_int
+        do it = 1_c_int, nt
+          dataspec_packed((2_c_int * coeff_index - 2_c_int) * nt + it) = &
+            solution_real((icol - 1_c_int) * nt + it)
+          dataspec_packed((2_c_int * coeff_index - 1_c_int) * nt + it) = &
+            solution_imag((icol - 1_c_int) * nt + it)
+        end do
+      end do
+
+      start_idx = start_idx + nblock
+    end do
+
+    deallocate( &
+      observed_real, observed_imag, basis_block_real, basis_block_imag, &
+      observed_block_real, observed_block_imag, solution_real, solution_imag)
+    ierror = 0_c_int
   end subroutine scalar_analysis_stub
 
   subroutine scalar_synthesis_stub( &
@@ -307,5 +414,133 @@ contains
       end do
     end do
   end subroutine solve_complex_system
+
+  logical function scalar_basis_cache_matches(ndgl, nloen, mu, ngptot, ntrunc) result(matches)
+    integer(c_int), intent(in) :: ndgl
+    integer(c_int), intent(in) :: nloen(ndgl)
+    real(c_double), intent(in) :: mu(ndgl)
+    integer(c_int), intent(in) :: ngptot, ntrunc
+
+    matches = .false.
+    if (basis_cache_ready == 0_c_int) then
+      return
+    end if
+    if (.not. allocated(cache_nloen) .or. .not. allocated(cache_mu)) then
+      return
+    end if
+    if (cache_ndgl /= ndgl .or. cache_ngptot /= ngptot .or. cache_ntrunc /= ntrunc) then
+      return
+    end if
+    if (.not. all(cache_nloen == nloen)) then
+      return
+    end if
+    if (.not. all(abs(cache_mu - mu) <= 1.0e-14_c_double)) then
+      return
+    end if
+    matches = .true.
+  end function scalar_basis_cache_matches
+
+  subroutine reset_scalar_basis_cache()
+    if (allocated(cache_nloen)) then
+      deallocate(cache_nloen)
+    end if
+    if (allocated(cache_mu)) then
+      deallocate(cache_mu)
+    end if
+    if (allocated(cache_basis_real)) then
+      deallocate(cache_basis_real)
+    end if
+    if (allocated(cache_basis_imag)) then
+      deallocate(cache_basis_imag)
+    end if
+    basis_cache_ready = 0_c_int
+    cache_ndgl = 0_c_int
+    cache_ngptot = 0_c_int
+    cache_ntrunc = -1_c_int
+  end subroutine reset_scalar_basis_cache
+
+  subroutine ensure_scalar_basis_cache(ndgl, nloen, mu, ngptot, ntrunc, ierror)
+    integer(c_int), intent(in) :: ndgl
+    integer(c_int), intent(in) :: nloen(ndgl)
+    real(c_double), intent(in) :: mu(ndgl)
+    integer(c_int), intent(in) :: ngptot, ntrunc
+    integer(c_int), intent(out) :: ierror
+
+    integer(c_int) :: ncoeff, nspec2, m, nblock, start_idx
+    integer(c_int) :: icol, ilat, coeff_index, ierr_local
+    real(c_double), allocatable :: packed_spec(:), basis_grid(:)
+    real(c_double), allocatable :: fourier_real(:), fourier_imag(:)
+
+    if (scalar_basis_cache_matches(ndgl, nloen, mu, ngptot, ntrunc)) then
+      ierror = 0_c_int
+      return
+    end if
+
+    call reset_scalar_basis_cache()
+
+    ncoeff = (ntrunc + 1_c_int) * (ntrunc + 2_c_int) / 2_c_int
+    nspec2 = 2_c_int * ncoeff
+
+    allocate(cache_nloen(ndgl))
+    allocate(cache_mu(ndgl))
+    allocate(cache_basis_real(ndgl * ncoeff))
+    allocate(cache_basis_imag(ndgl * ncoeff))
+    cache_nloen = nloen
+    cache_mu = mu
+    cache_basis_real = 0.0_c_double
+    cache_basis_imag = 0.0_c_double
+
+    start_idx = 1_c_int
+    do m = 0_c_int, ntrunc
+      nblock = ntrunc - m + 1_c_int
+      allocate(packed_spec(nspec2 * nblock))
+      allocate(basis_grid(ngptot * nblock))
+      allocate(fourier_real(ndgl * (m + 1_c_int) * nblock))
+      allocate(fourier_imag(ndgl * (m + 1_c_int) * nblock))
+
+      packed_spec = 0.0_c_double
+      do icol = 1_c_int, nblock
+        coeff_index = start_idx + icol - 1_c_int
+        packed_spec((2_c_int * coeff_index - 2_c_int) * nblock + icol) = 1.0_c_double
+      end do
+
+      call scalar_synthesis_stub( &
+        ndgl, nloen, mu, ngptot, ntrunc, nblock, packed_spec, basis_grid, ierr_local)
+      if (ierr_local /= 0_c_int) then
+        ierror = ierr_local
+        deallocate(packed_spec, basis_grid, fourier_real, fourier_imag)
+        call reset_scalar_basis_cache()
+        return
+      end if
+
+      call scalar_fourier_stub( &
+        ndgl, nloen, ngptot, m, nblock, basis_grid, fourier_real, fourier_imag, ierr_local)
+      if (ierr_local /= 0_c_int) then
+        ierror = ierr_local
+        deallocate(packed_spec, basis_grid, fourier_real, fourier_imag)
+        call reset_scalar_basis_cache()
+        return
+      end if
+
+      do ilat = 1_c_int, ndgl
+        do icol = 1_c_int, nblock
+          coeff_index = start_idx + icol - 1_c_int
+          cache_basis_real((ilat - 1_c_int) * ncoeff + coeff_index) = &
+            fourier_real(((ilat - 1_c_int) * (m + 1_c_int) + m) * nblock + icol)
+          cache_basis_imag((ilat - 1_c_int) * ncoeff + coeff_index) = &
+            fourier_imag(((ilat - 1_c_int) * (m + 1_c_int) + m) * nblock + icol)
+        end do
+      end do
+
+      deallocate(packed_spec, basis_grid, fourier_real, fourier_imag)
+      start_idx = start_idx + nblock
+    end do
+
+    basis_cache_ready = 1_c_int
+    cache_ndgl = ndgl
+    cache_ngptot = ngptot
+    cache_ntrunc = ntrunc
+    ierror = 0_c_int
+  end subroutine ensure_scalar_basis_cache
 
 end module scalar_backend
